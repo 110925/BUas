6474698e8e3b60e01527b8a3dce66a0b88ad5e2c
110925 <110925@st-maartenscollege.nl>
2022-12-11 21:33:18 +0100

    added some nice comments

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Tmpl8 {
+
+	class Surface;
+	class Game
+	{
+	public:
+		void SetTarget(Surface* surface) { screen = surface; }
+		void Init();
+		void Shutdown();
+		void Tick(float deltaTime);
+		void MouseUp(int button) { release = button; }
+		void MouseDown(int button) { click = button; }
+		void MouseMove(int x, int y) { mouseX = x, mouseY = y; }
+		void KeyUp(int key) { /* implement if you want to handle keys */ }
+		void KeyDown(int key) { /* implement if you want to handle keys */ }
+	private:
+		Surface* screen;
+		int mouseX, mouseY, click, release;//mouse variables
+		double delx, dely, angle, xvel, yvel, xpyt, ypyt, xypyt, minSpeed = 0.5, maxSpeed = 4; //velocity and angle calculation variables
+		int ballX = 300, ballY = 300; //x and y for ballz
+	};
+
+}; // namespace Tmpl8
--- game.cpp
+++ game.cpp
@@ -29,20 +29,18 @@ namespace Tmpl8
 
 	void Game::Tick(float deltaTime)
 	{
-		// clear the graphics window
-		screen->Clear(0);
+		screen->Clear(0); //clear the graphics window
+		ball.Draw(screen, ballX, ballY); //draw the ball
 
-
-		// draw a sprite
-		ball.Draw(screen, ballX, ballY);
-		screen->Line(mouseX, 0, mouseX, 720, 0xff0000);
-		screen->Line(0, mouseY, 1280, mouseY, 0xff0000);
-		if (click && !release) 
+		if (click && !release) //when mouse is held down
 		{
-			screen->Print("Mouseclick!", 10,10,0xff0000);
-			delx = (mouseX - ballX+25);
-			dely = (mouseY - ballY+25);
-			angle = atan(dely / delx);
+			screen->Print("Mouseclick!", 10, 10, 0xff0000);
+
+			delx = (mouseX - ballX + 25); //measures delta x
+			dely = (mouseY - ballY + 25); //measures delta y
+			angle = atan(dely / delx); //calculates the angle
+
+			//calculates the correct velocity 
 			if (delx >= 0)
 			{
 				xvel = cos(angle) * 5;
@@ -53,6 +51,8 @@ namespace Tmpl8
 				xvel = cos(angle) * -5;
 				yvel = sin(angle) * -5;
 			}
+
+			//calculates the speed using pytagoras 
 			if (delx < 0 || dely < 0) {
 				xpyt = delx * -1;
 				ypyt = dely * -1;
@@ -65,31 +65,29 @@ namespace Tmpl8
 			xpyt = pow(xpyt, 2);
 			ypyt = pow(ypyt, 2);
 			xypyt = sqrt(xpyt + ypyt) / 200;
+
+			//max and min speed
 			if (xypyt > maxSpeed) {
 				xypyt = maxSpeed;
 			}
 			if (xypyt < minSpeed) {
 				xypyt = minSpeed;
 			}
-			std::cout << "|" << xypyt;
-			screen->Line(ballX+25, ballY+25, mouseX, mouseY, 0xff0000);
+			screen->Line(ballX + 25, ballY + 25, mouseX, mouseY, 0xff0000); //line between ball and mouse
+		}
 
-		}	
-		if (release) 
+		if (release) //on release
 		{
 			click = 0;
 			release = 0;
 			xvel *= xypyt;
 			yvel *= xypyt;
 		}
-		if (click == 0) 
+
+		if (click == 0) //during release
 		{
 			ballX += xvel;
 			ballY += yvel;
 		}
-		
-		//std::cout << click; 
-
-
 	}
 };
